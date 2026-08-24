60febaf42a4ce035f7e40dd10d5d03c2cc167230
webitproff <webitproff@gmail.com>
2026-08-24 17:40:04 +0300

    version 1.1.4
    Added versioning and date comments to uninstallation SQL.

--- toc_page/setup/toc_page.uninstall.sql
+++ toc_page/setup/toc_page.uninstall.sql
@@ -2,5 +2,9 @@
 -- Removes plugin tables
 -- plugins/toc_page/setup/toc_page.uninstall.sql
 
+-- version 1.1.4
+-- Date Aug 24, 2026
+
+DROP TABLE IF EXISTS `cot_toc_page_i18n`;
 DROP TABLE IF EXISTS `cot_toc_page_items`;
 DROP TABLE IF EXISTS `cot_toc_page_trees`;
